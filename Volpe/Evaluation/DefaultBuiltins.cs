054e54e23cd9d7dce1f0cd8e90aefbb8760ae5bf
jiniux <53437303+jiniux@users.noreply.github.com>
2021-11-01 15:15:15 +0100

    Improve API.

--- Volpe/Evaluation/DefaultBuiltins.cs
+++ Volpe/Evaluation/DefaultBuiltins.cs
@@ -7,31 +7,18 @@ namespace Volpe.Evaluation
 {
     public static class DefaultBuiltins
     {
-        private static string GetValueRepresentation(Value v)
-        {
-            return v switch
-            {
-                Value.Number(var number) => number.ToString(CultureInfo.InvariantCulture),
-                Value.Void => "void",
-                Value.String(var value) => value, 
-                Value.FunctionReference(var name, var function) => $"<Function \"{name}\", {function.GetHashCode()}>",
-                
-                _ => throw new ArgumentOutOfRangeException(nameof(v), v, null)
-            };
-        }
-        
         public static (string, int, Func<Evaluator.Context, Value[], Value>)[] Io = new (string, int, Func<Evaluator.Context, Value[], Value>)[]
         {
             new ("println", 1, (context, values) =>
             {
-                string output = GetValueRepresentation(values[0]);
+                string output = values[0].Representation;
                 
                 Console.WriteLine(output);
                 
                 return Value.DefaultVoid;
             }),
             
-            new ("repr", 1, (context, values) => new Value.String(GetValueRepresentation(values[0])))
+            new ("repr", 1, (context, values) => new Value.String(values[0].Representation))
         };
     }
 }
--- Volpe/Evaluation/Value.cs
+++ Volpe/Evaluation/Value.cs
@@ -1,17 +1,38 @@
 using System;
+using System.Globalization;
 using Volpe.SyntaxAnalysis;
 
 namespace Volpe.Evaluation
 {
     public abstract record Value
     {
-        public record ToReturn(Value Value) : Value;
-        public record Number(double Value) : Value;
-        public record String(string Value) : Value;
-        public record Void : Value;
+        public record ToReturn(Value Value) : Value
+        {
+            public override string Representation => throw new InvalidOperationException();
+        }
 
-        public record FunctionReference(string Name, Function Function) : Value;
+        public record Number(double Value) : Value
+        {
+            public override string Representation => Value.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public record String(string Value) : Value
+        {
+            public override string Representation => Value;
+        }
+        
+        public record Void : Value
+        {
+            public override string Representation => "void";
+        }
+
+        public record FunctionReference(string Name, Function Function) : Value
+        {
+            public override string Representation => $"<Function \"{Name}\", {Function.GetHashCode()}>";
+        }
         
         public static readonly Void DefaultVoid = new Void();
+        
+        public abstract string Representation { get; }
     }
 }

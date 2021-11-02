ccef12244e7950f672f3fe1424cd15c704c90780
jiniux <53437303+jiniux@users.noreply.github.com>
2021-11-02 18:51:43 +0100

    Add REPL project for debugging

---
+++ Volpe.Repl/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using Volpe.Evaluation;
+using Volpe.Exceptions;
+using Volpe.LexicalAnalysis;
+using Volpe.SyntaxAnalysis;
+
+namespace Volpe.Repl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Volpe Language - REPL");
+
+            Scope scope = new Scope(DefaultBuiltins.Core);
+            
+            for (;;)
+            {
+                Console.Write(">> ");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    Parser parser = new Parser(new Lexer(input).GetTokenEnumerator().ToImmutableArray());
+                    Value[] results = parser.GetExpressionEnumerator()
+                        .Select(expr => new EvaluatorContext(expr, scope).Evaluate()).ToArray();
+
+                    for (int i = 0; i < results.Length; i++)
+                        Console.WriteLine($"[{i}] {results[i].Representation}");
+                }
+                catch (VolpeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}

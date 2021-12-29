f7b39a0084f5fc36316666285924ddd578cdc9ad
jiniux <53437303+jiniux@users.noreply.github.com>
2021-12-29 20:31:56 +0100

    Add timeit function to REPL.

--- Furball.Volpe.Repl/Program.cs
+++ Furball.Volpe.Repl/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Furball.Volpe.Evaluation;
@@ -38,6 +39,33 @@ namespace Furball.Volpe.Repl
                     
                     return Value.DefaultVoid;
                 }),
+                
+                new("timeit", 2, (context, values) =>
+                {
+                    if (values[0] is not Value.FunctionReference(_, var function))
+                        throw new InvalidValueTypeException(
+                            typeof(Value.FunctionReference), values[0].GetType(), context.Expression.PositionInText);
+                    
+                    if (values[1] is not Value.Number(var loops))
+                        throw new InvalidValueTypeException(
+                            typeof(Value.FunctionReference), values[0].GetType(), context.Expression.PositionInText);
+
+                    long lLoops = (long) loops;
+                    double totalMilliseconds = 0;
+
+                    for (long i = 0; i < lLoops; i++)
+                    {
+                        Stopwatch stopwatch = new Stopwatch();
+
+                        stopwatch.Start();
+                        function.Invoke(context, Array.Empty<Value>());
+                        stopwatch.Stop();
+
+                        totalMilliseconds += stopwatch.Elapsed.TotalMilliseconds;
+                    }
+                    
+                    return new Value.Number(totalMilliseconds / loops);
+                })
             }).ToArray());
             
             for (;;)
